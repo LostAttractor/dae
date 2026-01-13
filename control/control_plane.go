350462042c38b30eb725ace80127931b84d29dd6
ChaosAttractor <lostattractor@gmail.com>
2026-01-13 10:36:07 +0800

    fix: udp data buffer forget to put back
    Co-Authored-By: ppdragon16 <4575408+ppdragon16@users.noreply.github.com>

--- control/control_plane.go
+++ control/control_plane.go
@@ -772,9 +772,6 @@ func (c *ControlPlane) Serve(readyChan chan<- bool, listener *Listener) (err err
 			src = common.ConvergeAddrPort(src)
 			dst = common.ConvergeAddrPort(dst)
 
-			data := pool.GetBuffer(n)
-			copy(data, buf[:n])
-
 			/// Handle DNS
 			// To keep consistency with kernel program, we only sniff DNS request sent to 53.
 			if dst.Port() == 53 {
@@ -785,7 +782,7 @@ func (c *ControlPlane) Serve(readyChan chan<- bool, listener *Listener) (err err
 				}
 				if routingResult.Must == 0 {
 					var dnsMessage dnsmessage.Msg
-					if err := dnsMessage.Unpack(data); err == nil {
+					if err := dnsMessage.Unpack(buf[:n]); err == nil {
 						c.dnsController.Handle(&dnsMessage, &udpRequest{
 							src:           src,
 							dst:           dst,
@@ -795,6 +792,9 @@ func (c *ControlPlane) Serve(readyChan chan<- bool, listener *Listener) (err err
 					}
 				}
 			}
+
+			data := pool.GetBuffer(n)
+			copy(data, buf[:n])
 
 			// Debug:
 			// t := time.Now()
