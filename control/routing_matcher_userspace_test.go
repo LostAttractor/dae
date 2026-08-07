db2dadc0612b7d1778c93f0d5afb7ee48090fa77
ChaosAttractor <lostattractor@gmail.com>
2026-08-07 14:33:32 +0800

    fix: sync ifname updates with userspace routing

--- control/routing_matcher_userspace_test.go
+++ control/routing_matcher_userspace_test.go
@@ -6,10 +6,127 @@
 package control
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/daeuniverse/dae/common/consts"
+	"github.com/daeuniverse/dae/pkg/config_parser"
 )
+
+func buildIfnameRoutingMatcher(t *testing.T) (*RoutingMatcher, *RoutingMatcherBuilder) {
+	t.Helper()
+
+	builder, err := NewRoutingMatcherBuilder(
+		[]*config_parser.RoutingRule{{
+			AndFunctions: []*config_parser.Function{{
+				Name:   consts.Function_IfName,
+				Params: []*config_parser.Param{{Val: "test0"}},
+			}},
+			Outbound: config_parser.Function{Name: "matched"},
+		}},
+		map[string]uint8{
+			"matched":  uint8(consts.OutboundUserDefinedMin),
+			"fallback": uint8(consts.OutboundDirect),
+		},
+		nil,
+		"fallback",
+		nil,
+	)
+	if err != nil {
+		t.Fatalf("NewRoutingMatcherBuilder: %v", err)
+	}
+	matcher, err := builder.BuildUserspace()
+	if err != nil {
+		t.Fatalf("BuildUserspace: %v", err)
+	}
+	return matcher, builder
+}
+
+func matchIfindex(t *testing.T, matcher *RoutingMatcher, ifindex uint32) consts.OutboundIndex {
+	t.Helper()
+
+	addr := make([]byte, 16)
+	outbound, _, _, err := matcher.Match(
+		addr,
+		addr,
+		0,
+		0,
+		consts.IpVersion_4,
+		consts.L4ProtoType_TCP,
+		"",
+		[16]uint8{},
+		ifindex,
+		0,
+		make([]byte, 16),
+	)
+	if err != nil {
+		t.Fatalf("Match: %v", err)
+	}
+	return outbound
+}
+
+func TestRoutingMatcherDynamicIfname(t *testing.T) {
+	matcher, builder := buildIfnameRoutingMatcher(t)
+
+	if got := matchIfindex(t, matcher, 7); got != consts.OutboundDirect {
+		t.Fatalf("unresolved ifname selected outbound %v, want %v", got, consts.OutboundDirect)
+	}
+	builder.storeIfindex(0, 7)
+	if got := matchIfindex(t, matcher, 7); got != consts.OutboundUserDefinedMin {
+		t.Fatalf("resolved ifname selected outbound %v, want %v", got, consts.OutboundUserDefinedMin)
+	}
+	builder.storeIfindex(0, 0)
+	if got := matchIfindex(t, matcher, 7); got != consts.OutboundDirect {
+		t.Fatalf("deleted ifname selected outbound %v, want %v", got, consts.OutboundDirect)
+	}
+}
+
+func TestRoutingMatcherConcurrentIfnameUpdate(t *testing.T) {
+	matcher, builder := buildIfnameRoutingMatcher(t)
+
+	const iterations = 10_000
+	start := make(chan struct{})
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		<-start
+		for i := 0; i < iterations; i++ {
+			builder.storeIfindex(0, uint32(i%2+7))
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		<-start
+		addr := make([]byte, 16)
+		mac := make([]byte, 16)
+		for i := 0; i < iterations; i++ {
+			outbound, _, _, err := matcher.Match(
+				addr,
+				addr,
+				0,
+				0,
+				consts.IpVersion_4,
+				consts.L4ProtoType_TCP,
+				"",
+				[16]uint8{},
+				7,
+				0,
+				mac,
+			)
+			if err != nil {
+				t.Errorf("Match: %v", err)
+				return
+			}
+			if outbound != consts.OutboundUserDefinedMin && outbound != consts.OutboundDirect {
+				t.Errorf("unexpected outbound %v", outbound)
+				return
+			}
+		}
+	}()
+	close(start)
+	wg.Wait()
+}
 
 func newSkipWhileNoaliveMatcher(usable *bool, gotArgs *struct {
 	outbound  uint8
@@ -17,6 +134,7 @@ func newSkipWhileNoaliveMatcher(usable *bool, gotArgs *struct {
 	ipVersion consts.IpVersionType
 }) *RoutingMatcher {
 	m := &RoutingMatcher{
+		rulesMu: new(sync.RWMutex),
 		matches: []bpfMatchSet{
 			{
 				Type:             uint8(consts.MatchType_Port),
@@ -99,6 +217,7 @@ func TestRoutingMatcherSkipWhileNoaliveOnDirect(t *testing.T) {
 	// participate in connectivity checks and are always usable.
 	usable := false
 	m := &RoutingMatcher{
+		rulesMu: new(sync.RWMutex),
 		matches: []bpfMatchSet{
 			{
 				Type:             uint8(consts.MatchType_Port),
