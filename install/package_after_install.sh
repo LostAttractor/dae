ffeba870e8c7164453e194c333affe60237dcf26
ChaosAttractor <lostattractor@gmail.com>
2026-08-14 09:57:02 +0800

    fix(install): restart only active service

--- install/package_after_install.sh
+++ install/package_after_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $(command -v systemctl) ]; then
+if command -v systemctl >/dev/null 2>&1; then
 	systemctl daemon-reload
 
-	if [ $(systemctl is-active dae) ]; then
-		systemctl restart dae.service
+	if systemctl is-active --quiet dae.service; then
+		if ! systemctl restart dae.service; then
+			exit 1
+		fi
 		echo "Restarting dae service, it might take a while."
 	fi
 fi
